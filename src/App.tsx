a82fed349393fe5614e0e1489c920bea9e898ace
Marcin Adamiak <marcin_89@outlook.com>
2024-01-27 11:30:18 +0100

    ADD: mui, google fonts and navabar

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,24 @@
 import React from 'react';
 import { Route, Routes } from 'react-router';
+import { Container } from '@mui/material';
 import Home from './components/pages/home/Home';
 import About from './components/pages/about/About';
 import Rules from './components/pages/rules/Rules';
 import NotFound from './components/pages/404/NotFound';
+import Navbar from './components/views/Navbar/Navbar';
 
 const App = () => {
   return (
     <main>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/rules' element={<Rules />} />
-        <Route path='*' element={<NotFound />} />
-      </Routes>
+      <Container>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/rules' element={<Rules />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+      </Container>
     </main>
   );
 };
